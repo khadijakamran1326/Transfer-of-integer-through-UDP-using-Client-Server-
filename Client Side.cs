cfd23c9c38e8f9dfae51b20fc6ad22ed504ee5c1
khadijakamran1326 <khadijakamran746@gmail.com>
2025-10-23 12:54:24 +0500

    Add files via upload

---
+++ Client Side.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPClient
+{
+    static void Main()
+    {
+        UdpClient client = new UdpClient();
+        client.Connect("127.0.0.1", 9000);
+
+        Console.Write("Enter integer to send to server(client side): ");
+        int clientValue = int.Parse(Console.ReadLine());
+
+        byte[] data = Encoding.ASCII.GetBytes(clientValue.ToString());
+        client.Send(data, data.Length);
+
+        IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 0);
+        byte[] receivedData = client.Receive(ref serverEP);
+        string result = Encoding.ASCII.GetString(receivedData);
+
+        Console.WriteLine($"Sum received from server:{result}");
+        client.Close();
+    }
+}
---
+++ Server side.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPServer
+{
+    static void Main()
+    {
+        UdpClient server = new UdpClient(9000);
+        Console.WriteLine("Server waiting for client number...");
+
+        IPEndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
+        byte[] data = server.Receive(ref clientEP);
+
+        int clientValue = int.Parse(Encoding.ASCII.GetString(data));
+        Console.WriteLine($"Received value from client:{clientValue}");
+
+        Console.Write("Enter another integer:(server side):");
+        int serverValue = int.Parse(Console.ReadLine());
+
+        int sum = clientValue + serverValue;
+        Console.WriteLine($"Sum = {sum}");
+
+        byte[] sendData = Encoding.ASCII.GetBytes(sum.ToString());
+        server.Send(sendData, sendData.Length, clientEP);
+
+        Console.WriteLine("Sum sent back to client.");
+        server.Close();
+    }
+}
